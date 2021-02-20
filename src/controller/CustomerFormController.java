5cdd4276cf47cfd7281bae83ba25d62e73f5f8c9
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-02-20 10:07:21 +0530

    3

--- src/controller/CustomerFormController.java
+++ src/controller/CustomerFormController.java
@@ -1,4 +1,52 @@
 package controller;
 
+import database.ThogaKade;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Customer;
+
 public class CustomerFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField ttSalary;
+    public TableView table;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+
+    public void initialize(){
+
+        colId.setCellValueFactory(new PropertyValueFactory("id"));
+        colName.setCellValueFactory(new PropertyValueFactory("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory("salary"));
+
+        loadAllCustomers();
+    }
+
+    private void loadAllCustomers() {
+        table.setItems(FXCollections.observableArrayList(ThogaKade.customerTable));
+    }
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer customer = new Customer(
+                txtId.getText(),
+                txtName.getText(),
+                txtAddress.getText(),
+                Double.parseDouble(ttSalary.getText())
+        );
+
+        if (ThogaKade.customerTable.add(customer)){
+           new Alert(Alert.AlertType.CONFIRMATION, "Success!", ButtonType.OK).show();
+            System.out.println(ThogaKade.customerTable);
+        }else{
+            new Alert(Alert.AlertType.ERROR, "Try Again", ButtonType.CLOSE).show();
+        }
+
+
+    }
 }
--- src/database/ThogaKade.java
+++ src/database/ThogaKade.java
@@ -6,4 +6,12 @@ import java.util.ArrayList;
 
 public class ThogaKade {
     public static ArrayList<Customer> customerTable= new ArrayList();
+
+    static {
+      customerTable.add(new Customer("C-001","Nimal","Colombo",25000));
+      customerTable.add(new Customer("C-002","Bandara","Panadura",45000));
+      customerTable.add(new Customer("C-003","Wasantha","Kalutara",78500));
+      customerTable.add(new Customer("C-004","Kalpa","Galle",24500));
+      customerTable.add(new Customer("C-005","Samantha","Matara",16500));
+    }
 }
